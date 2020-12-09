7bcc2469c79844fbc5c39b157306716338ea0476
Jhonata Caio <jhonatacaiob@gmail.com>
2020-12-09 18:23:24 -0300

    Altera o codigo para compatibilidade em outros sistemas e o nome de algumas variaveis

--- principal.py
+++ principal.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from decouple import config
 import requests
 from PIL import Image, ImageFilter
@@ -10,10 +6,11 @@ import datas
 import os
 
 
+print()
 username = config("USER")
-URL = "https://www.tapmusic.net/collage.php?user={}&size=5x5&caption=true&type=".format(username)
-CAMINHO = "C:\\" + os.path.join('Users', 'Jhonata','Desktop', "Projeto Jornada Musical", "data") + "\\"
-	
+URL = f"https://www.tapmusic.net/collage.php?user={username}&size=5x5&caption=true&type="
+CAMINHO = os.path.join(os.getcwd(), 'data')
+print(os.path.join(os.getcwd(), 'data'))
 
 
 def pega_imagem(tipo):
@@ -22,34 +19,31 @@ def pega_imagem(tipo):
     return im
    
 
-def salva_imagem(Imagem, pasta, endereco):
-    if not os.path.exists(pasta):
-        os.makedirs(pasta)
-    Imagem.save(pasta + endereco + ".jpg")
+def salva_imagem(Imagem, diretorio, nome_arquivo):
+	if not os.path.exists(diretorio):
+		os.makedirs(diretorio)
 
+	Imagem.save(f"{diretorio}\\{nome_arquivo}.jpg")
+	print(f"Imagem salva como: {diretorio}\\{nome_arquivo}.jpg")
 
-print("RODANDO...")
 
 if(datas.dia_semana_hoje() == 5):
-	pasta = CAMINHO + "SEMANAL\\"  + str(datas.ano()) + "\\" + str(datas.mes_hoje()) + "\\"
-	
+	diretorio = os.path.join(CAMINHO, "SEMANAL", str(datas.ano()), str(datas.mes_hoje()))
+
 	imagem = pega_imagem("7day")
-	salva_imagem(imagem, pasta, str(datas.dia_hoje()))
+	salva_imagem(imagem, diretorio, datas.dia_hoje())
 
 
 if(datas.dia_hoje() == 1):
-	pasta = CAMINHO  + "MENSAL\\"  + str(datas.ano()) + "\\"
+	diretorio = os.path.join(CAMINHO, "MENSAL", str(datas.ano()))
 		
 	imagem = pega_imagem("1month")
-	salva_imagem(imagem, pasta, str(datas.mes_hoje() - 1))
+	salva_imagem(imagem, diretorio, (datas.mes_hoje() - 1))
 
 
 if(datas.mes_hoje() == 1 and
 	datas.dia_hoje() == 1):
-	pasta = CAMINHO + "ANUAL\\" 
+	diretorio = os.path.join(CAMINHO, "ANUAL")
 
 	imagem = pega_imagem("12month")
-	salva_imagem(imagem, pasta, str(datas.ano()-1))
-
-
-print("FINALIZADO")
+	salva_imagem(imagem, diretorio, (datas.ano()-1))
